6eba2c87fd91232b064a0dc75fa413db246dc628
Narasimha <Narasimha@192.168.4.8>
2020-02-22 11:18:47 +0530

    addedmemories

---
+++ dolls/src/main/java/MavenSeleniumFramework/dolls/App.java
@@ -0,0 +1,13 @@
+package MavenSeleniumFramework.dolls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dolls/src/test/java/dollstype/Differenttype.java
@@ -0,0 +1,5 @@
+package dollstype;
+
+public class Differenttype {
+
+}
